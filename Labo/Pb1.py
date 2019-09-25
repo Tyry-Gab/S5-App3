1204aca768f34fee5e36f297de46196ef2fba405
glesssard <lesg2605@usherbrooke.ca>
2019-09-25 11:51:46 -0400

    Pb1 done

--- Labo/Pb1.py
+++ Labo/Pb1.py
@@ -2,25 +2,22 @@ import matplotlib.pyplot as mplt
 import numpy as np
 from scipy import signal
 
+
 # Test commit push
-N = 42
+N = 256
 n = range(0, N)
 x1 = np.empty(N)
-x1_hanning = x1
 x2 = np.empty(N)
-x2_hanning = x2
 x3 = np.empty(N)
-x3_hanning = x3
-Hanning_Window = np.empty(N)
+win = np.hanning(N)
 
 F_by_Fe = np.empty(N)
+W_normalised = np.empty(N)
 
-W_normalised = np.hanning(N)
 
 
 for i in n:
     x1[i] = np.sin(0.1*np.pi*i + np.pi/4)
-    x1_hanning[i] = x1[i] * Hanning_Window[i]
     x2[i] = (-1)**i
     if i == 10:
         x3[i] = 1
@@ -45,6 +42,16 @@ mplt.stem(n, x3)
 mplt.stem(W_normalised, np.abs(np.fft.fft(x3)))
 #mplt.show()
 
-mplt.stem(np.hanning(42))
+
+mplt.close('all')
+
+
+mplt.stem(x1*win)
 mplt.show()
+mplt.stem(x2*win)
+mplt.show()
+mplt.stem(x3*win)
+mplt.show()
+
+
 
