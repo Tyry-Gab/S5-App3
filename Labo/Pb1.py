3245cf43145068404dc6ffb9f77a32481f48af9e
glesssard <lesg2605@usherbrooke.ca>
2019-09-25 10:07:26 -0400

    Graph of first function

--- Labo/Pb1.py
+++ Labo/Pb1.py
@@ -1,5 +1,21 @@
-import matplotlib
-import numpy
-import scipy
+import matplotlib.pyplot as mplt
+import numpy as np
+from scipy import signal
 
 # Test commit push
+N = 40
+n = range(0, N)
+x1 = np.empty(N)
+
+for i in n:
+    x1[i] = np.sin(0.1*np.pi*i + np.pi/4)
+
+
+mplt.stem(n, x1)
+mplt.show()
+mplt.stem(n, np.abs( np.fft.fft(x1)))
+mplt.show()
+
+
+
+
