e8cc2bfb37989f945fed6de6b43fc661ed23cfe5
glesssard <lesg2605@usherbrooke.ca>
2019-09-25 11:38:17 -0400

    Hanning

--- Labo/Pb1.py
+++ Labo/Pb1.py
@@ -3,49 +3,48 @@ import numpy as np
 from scipy import signal
 
 # Test commit push
-N = 40
+N = 42
 n = range(0, N)
 x1 = np.empty(N)
+x1_hanning = x1
 x2 = np.empty(N)
+x2_hanning = x2
 x3 = np.empty(N)
+x3_hanning = x3
+Hanning_Window = np.empty(N)
 
-m1 = np.empty(N)
-m2 = np.empty(N)
-m3 = np.empty(N)
+F_by_Fe = np.empty(N)
 
-We1 = 2*np.pi/20
-We2 = 2*np.pi/2
-We3 = 2*np.pi/40
+W_normalised = np.hanning(N)
+
+
 for i in n:
     x1[i] = np.sin(0.1*np.pi*i + np.pi/4)
+    x1_hanning[i] = x1[i] * Hanning_Window[i]
     x2[i] = (-1)**i
     if i == 10:
         x3[i] = 1
     else:
         x3[i] = 0
-    m1 = We1 * i / N
-    m2 = We2 * i / N
-    m3 = We3 * i / N
+    F_by_Fe[i] = i/N
+    W_normalised[i] = 2*np.pi*F_by_Fe[i]
 
-m = 0
 
 mplt.stem(n, x1)
-mplt.show()
-mplt.stem(m1, np.abs(np.fft.fft(x1)))
-mplt.show()
+#mplt.show()
+mplt.stem(W_normalised, np.abs(np.fft.fft(x1)))
+#mplt.show()
 
 mplt.stem(n, x2)
-mplt.show()
-mplt.stem(m2, np.abs(np.fft.fft(x2)))
-mplt.show()
+#mplt.show()
+mplt.stem(W_normalised, np.abs(np.fft.fft(x2)))
+#mplt.show()
 
 mplt.stem(n, x3)
+#mplt.show()
+mplt.stem(W_normalised, np.abs(np.fft.fft(x3)))
+#mplt.show()
+
+mplt.stem(np.hanning(42))
 mplt.show()
-mplt.stem(m3, np.abs(np.fft.fft(x3)))
-mplt.show()
-
-
-
-
-
 
