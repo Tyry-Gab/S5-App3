8f418478c34e90a3ed7bb2f356f54861b761a2ff
glesssard <lesg2605@usherbrooke.ca>
2019-09-28 12:40:04 -0400

    Fixing commit issues

--- Problematique/main_script.py
+++ Problematique/main_script.py
@@ -2,25 +2,46 @@ import matplotlib.pyplot as plt
 import numpy as np
 import wave
 import sys
+from Problematique import SoundsUtil
 
-spf = wave.open('note_guitare_LAd.wav','r')
 
-#Extract Raw Audio from Wav File
-signal = spf.readframes(-1)
-signal = np.fromstring(signal, 'Int16')
-fs = spf.getframerate()
-
-#If Stereo
+# Opening file, continuing only if mono
+spf = wave.open('note_guitare_LAd.wav', 'r')
 if spf.getnchannels() == 2:
     print("Just mono files")
     sys.exit(0)
 
+# Playing sound
+#SoundsUtil.playSound(spf)
 
-Time=np.linspace(0, len(signal)/fs, num=len(signal))
 
+# Extract Raw Audio from Wav File
+signal = spf.readframes(-1)
+signal = np.fromstring(signal, 'Int16')
+fs = spf.getframerate()
+
+# Plotting
+Time = np.linspace(0, len(signal)/fs, num=len(signal))
+
+# Base signal plotting
 plt.figure(1)
 plt.title('Guitar A# signal')
 plt.xlabel('Time (s)')
 plt.ylabel('Amplitude')
-plt.plot(Time,signal)
+plt.plot(Time, signal)
 plt.show()
+plt.close('all')
+
+# Applying window to signal, order needs to be lower than 16000 based on signal
+N = len(signal)
+window = np.hanning(N)
+
+windowed_singal = signal * window
+
+plt.figure()
+plt.title('Windowed A#')
+plt.stem(windowed_singal)
+plt.show()
+plt.close('all')
+
+print("Done!")
