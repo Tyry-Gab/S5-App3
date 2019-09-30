e8a93058a262960644589c14bbf0b408b7d92ef3
Thierry Constantin <thierry.constantin@usherbrooke.ca>
2019-09-29 23:05:28 -0400

    A lot of nice things

--- Problematique/main_script.py
+++ Problematique/main_script.py
@@ -1,25 +1,67 @@
 import matplotlib.pyplot as plt
 import numpy as np
-import wave
+import scipy.io.wavfile as wavfile
 import sys
 #import SoundsUtil
-from problematique_helper import *
+from Problematique.problematique_helper import *
 
 
 # Opening file, continuing only if mono
-spf = wave.open('note_guitare_LAd.wav', 'r')
-if spf.getnchannels() == 2:
-    print("Just mono files")
-    sys.exit(0)
+signal, fs, N = param_wav('note_guitare_LAd.wav')
 
-# Playing sound
-#SoundsUtil.playSound(spf)
+# Find most significative frequencies from their amplitude (f (Hz), amplitude, phase (rad))
+f_peaks, amplitude, phase = get_useful_sine_waves_params(signal, fs, N)
+
+# todo Find the right number of coefficients
+rif_filter = create_RIF_equi_coeff(1000)
+
+convolution = convolution(signal, rif_filter)
+
+n = np.arange(len(convolution))
+
+#Generate notes: (A# is the base note)
+
+do_signal = generate_signal(amplitude, f_peaks, fs, phase, n, -10)
+re_signal = generate_signal(amplitude, f_peaks, fs, phase, n, -8)
+mi_signal = generate_signal(amplitude, f_peaks, fs, phase, n, -6)
+fa_signal = generate_signal(amplitude, f_peaks, fs, phase, n, -5)
+sol_signal = generate_signal(amplitude, f_peaks, fs, phase, n, -3)
+la_signal = generate_signal(amplitude, f_peaks, fs, phase, n, -1)
+si_signal = generate_signal(amplitude, f_peaks, fs, phase, n, 1)
+do_signal2 = generate_signal(amplitude, f_peaks, fs, phase, n, 2)
+
+output_signal = generate_note(do_signal, convolution)
+output_signal = np.append(output_signal, generate_note(re_signal, convolution))
+output_signal = np.append(output_signal, generate_note(mi_signal, convolution))
+output_signal = np.append(output_signal, generate_note(fa_signal, convolution))
+output_signal = np.append(output_signal, generate_note(sol_signal, convolution))
+output_signal = np.append(output_signal, generate_note(la_signal, convolution))
+output_signal = np.append(output_signal, generate_note(si_signal, convolution))
+output_signal = np.append(output_signal, generate_note(do_signal2, convolution))
+
+wavfile.write("Test_Gamme.wav",fs,np.int16(np.real(output_signal)))
+
+# 5th symphony
+symph_signal = generate_half_silence()
+symph_signal = np.append(symph_signal, generate_half_note(sol_signal, convolution))
+symph_signal = np.append(symph_signal, generate_half_note(sol_signal, convolution))
+symph_signal = np.append(symph_signal, generate_half_note(sol_signal, convolution))
+symph_signal = np.append(symph_signal, generate_double_note(mi_signal, convolution))
+symph_signal = np.append(symph_signal, generate_half_silence())
+symph_signal = np.append(symph_signal, generate_half_note(fa_signal, convolution))
+symph_signal = np.append(symph_signal, generate_half_note(fa_signal, convolution))
+symph_signal = np.append(symph_signal, generate_half_note(fa_signal, convolution))
+symph_signal = np.append(symph_signal, generate_double_note(re_signal, convolution))
+
+wavfile.write("Fifth_Symphony.wav",fs,np.int16(np.real(symph_signal)))
 
 
-# Extract Raw Audio from Wav File
-signal = spf.readframes(-1)
-signal = np.fromstring(signal, 'Int16')
-fs = spf.getframerate()
+# Test to look if sine seems good
+test_sine = generate_sine_wave(amplitude[3], f_peaks[3], fs, phase[3], n)
+plt.figure(0)
+plt.plot(n, test_sine)
+plt.show()
+
 
 # Plotting
 Time = np.linspace(0, len(signal)/fs, num=len(signal))
@@ -33,7 +75,6 @@ plt.plot(Time, signal)
 plt.show()
 plt.close('all')
 
-N = spf.getnframes()
 w_normalized = np.linspace(0, N-1, N)
 w_normalized = 2*np.pi*w_normalized/N
 plt.plot(w_normalized, np.abs(np.fft.fft(signal)))
@@ -66,6 +107,28 @@ while len(highest_32_sin_Hz) < 32:
             highest_32_sin_Hz.append((frequency, sorted_freq[i], f))
     i += 1
 
+plt.figure()
+plt.title('Half Windowed transformed')
+db_signal = 20*np.log10(amp_abs_fft_signal / amp_abs_fft_signal[highest_32_sin_Hz[31][2]])
+plt.plot(w_normalized[0:elem_to_take], db_signal[0:elem_to_take])
+plt.show()
+plt.close('all')
 
+
+# Create all 32 sinus again
+sin_32 = []
+reconstructed_signal = np.empty(N)
+i = 0
+for sinus in highest_32_sin_Hz:
+    temp_sinus = sinus[1] * np.sin(windowed_signal[sinus[2]] * Time * sinus[2])
+    reconstructed_signal += temp_sinus
+    sin_32.append(temp_sinus)
+
+
+plt.figure()
+plt.title('Sinus')
+plt.plot(Time, sin_32[1])
+plt.show()
+plt.close('all')
 
 print("Done!")
--- Problematique/problematique_helper.py
+++ Problematique/problematique_helper.py
@@ -1,3 +1,90 @@
+import wave
+import sys
+import numpy as np
+import scipy.signal as sp
+
+TEMPO = 16000
+
+def param_wav(wav_file):
+    """
+    :param wav_file: The filename of the wav file to open
+    :type wav_file: String
+    :returns: signal: Temporal signal, fs: Framerate of signal, N: Number of samples
+    :rtype: int16 array, float, int
+    """
+    spf = wave.open(wav_file, 'r')
+    if spf.getnchannels() == 2:
+        print("Just mono files")
+        sys.exit(0)
+
+    # Extract Raw Audio from Wav File
+    signal = spf.readframes(-1)
+    signal = np.fromstring(signal, 'Int16')
+    fs = spf.getframerate()
+
+    N = spf.getnframes()
+    print("Fréquence d'échantillonage: " + str(fs))
+    print("Nombre d'échantillons: " + str(N))
+
+    return signal, fs, N
+
+def get_useful_sine_waves_params(signal, fs, N):
+    # Only first half is kept as information is only mirrored in second half.
+    fft_result = np.fft.fft(signal * np.hanning(N))[0:N//2]
+
+    # todo perform on windowed or original??
+    angle = np.angle(np.fft.fft(signal))[0:N//2]
+
+    # TODO look effects of params on results
+    f_peaks, d = sp.find_peaks(fft_result, height=0.0032*max(fft_result), distance=1, width=1)
+
+    amplitude = fft_result[f_peaks]
+    phase = angle[f_peaks]
+
+    f_peaks = f_peaks * fs / N
+    return f_peaks, amplitude, phase
+
+def create_RIF_equi_coeff(nb_coeffs):
+    return [1/nb_coeffs] * nb_coeffs
+
+# Redresse le signal (abs) et applique un filtre sur ce dernier.
+def convolution(signal, filter):
+    # todo Verify mode between "full" (default), "same", "valid"
+    return np.convolve(filter, abs(signal), "valid")
+
+def generate_sine_wave(amplitude, f, fs, phase, n, ecart_note = 0):
+    return np.multiply(amplitude, (np.sin((2 * np.pi * f * n / fs) * (2 ** (ecart_note / 12)) + phase)))
+
+def generate_signal(amplitudes, f_peaks, fs, phases, n, ecart_note = 0):
+    sine_waves_sum = [0]
+    for i in range(len(f_peaks)):
+        sine_waves_sum += generate_sine_wave(amplitudes[i], f_peaks[i], fs, phases[i], n, ecart_note)
+    return sine_waves_sum
+
+def generate_half_note(note_signal, filtered_signal):
+    output = np.multiply(note_signal, filtered_signal)
+    # Lower sound volume
+    output = output / (max(output) / 10000)
+    output = np.resize(output, (1, TEMPO))
+    return output[0]
+
+def generate_note(note_signal, filtered_signal):
+    output = np.multiply(note_signal, filtered_signal)
+    # Lower sound volume
+    output = output / (max(output) / 10000)
+    output = np.resize(output, (1, 2*TEMPO))
+    return output[0]
+
+def generate_double_note(note_signal, filtered_signal):
+    output = np.multiply(note_signal, filtered_signal)
+    # Lower sound volume
+    output = output / (max(output) / 10000)
+    output = np.resize(output, (1, 4*TEMPO))
+    return output[0]
+
+def generate_half_silence():
+    return [0] * TEMPO
+
 
 def is_in_list_with_tolerance(some_list, element_to_add, accept_factor):
     if element_to_add == None or element_to_add == 0.0:
@@ -8,4 +95,5 @@ def is_in_list_with_tolerance(some_list, element_to_add, accept_factor):
         if element_to_add >= element_l and element_to_add <= element_h:
             return False
     return True
+
 
